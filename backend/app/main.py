ca8e9762b4191e772ab72f05763fb10184c75766
prajyot001 <prajyotpatil17004@gmail.com>
2026-02-27 01:17:58 +0530

    last test

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://insider-threat-seven.vercel.app/login",
+        "https://insider-threat-seven.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],

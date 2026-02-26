9c727e0e1ccffeedd6cae98b5a3755077e795223
prajyot001 <prajyotpatil17004@gmail.com>
2026-02-26 19:26:37 +0530

    ready for deployment

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,7 @@
+import os
 import uvicorn
+from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)

93a51fe16640be78272a7841256015b03084026d
prajyot001 <prajyotpatil17004@gmail.com>
2026-02-26 21:44:10 +0530

    upadated

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict
+    allow_origins=[
+        "http://localhost:3000",
+        "https://insider-backend-jggo.onrender.com/.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

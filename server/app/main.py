9c274b195559c2869187e97a0f74edfb4330939a
prajyot001 <prajyotpatil17004@gmail.com>
2026-02-27 10:46:35 +0530

    last time

--- server/app/main.py
+++ server/app/main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from .config import APP_NAME
 from .routes import health, logs
 
-app = FastAPI(
-    title=APP_NAME,
-    docs_url=None,
-    redoc_url=None
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*" ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 app.include_router(health.router, prefix="/health")
 app.include_router(logs.router, prefix="/server")
